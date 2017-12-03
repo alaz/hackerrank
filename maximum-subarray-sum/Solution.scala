e1d14876bb345cc39a5a5e61cc96cf41888c0a27
Alexander Azarov <azarov@osinka.ru>
2017-12-03 14:40:45 +0200

    ✨ Maximum subarray sum, FP approach
    https://www.hackerrank.com/challenges/maximum-subarray-sum

---
+++ maximum-subarray-sum/Solution.scala
@@ -0,0 +1,21 @@
+// https://www.hackerrank.com/challenges/maximum-subarray-sum
+object Solution {
+  import scala.math.max
+  import scala.collection.immutable.TreeSet
+
+  def maxSubSumMod(input: Stream[Long], m: Long): Long =
+    input.foldLeft( (0L, 0L, TreeSet.empty[Long]) ) { case ((result, prev, set), x) =>
+      val cur = (prev + x) % m
+      val best = for { greater <- set.range(cur+1, m).headOption } yield (cur - greater + m) % m
+      (max(best getOrElse cur, result), cur, set + cur)
+    }._1
+
+  def main(args: Array[String]) {
+    val scanner = new java.util.Scanner(System.in)
+    for {_ <- 0 until scanner.nextInt} {
+      val (n, m) = (scanner.nextInt, scanner.nextLong)
+      val input = Stream.fill(n)(scanner.nextLong)
+      println(maxSubSumMod(input, m))
+    }
+  }
+}
