b3f539cf4a1bcb8da522db5843c6a19a5211204e
Alexander Azarov <azarov@osinka.ru>
2018-12-07 22:22:22 +0200

    ✨ The PADS
    https://www.hackerrank.com/challenges/the-pads

---
+++ the-pads.sql
@@ -0,0 +1,10 @@
+-- https://www.hackerrank.com/challenges/the-pads
+
+SELECT CONCAT(Name, '(', SUBSTRING(Occupation, 1, 1), ')')
+FROM OCCUPATIONS
+ORDER BY Name;
+
+SELECT CONCAT('There are a total of ', COUNT(*), ' ', LOWER(Occupation), 's.')
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY COUNT(*), Occupation;
