335a70a15bbc62b833a75bdcd2ca3ae471b13ac0
Alexander Azarov <azarov@osinka.ru>
2019-03-06 18:40:52 +0200

    ✨ Merge Sort
    https://www.hackerrank.com/challenges/ctci-merge-sort

--- ctci-merge-sort.scala
+++ ctci-merge-sort.scala
@@ -2,45 +2,36 @@
 https://www.hackerrank.com/challenges/ctci-merge-sort
 */
 object Solution {
-  // insertion sort
-  // O(n²•log n)
-  def calc(d: Int, it: Iterator[Int]) = {
-    val a = Array.ofDim[Int](d)
+  // merge sort
+  def calc(a: Array[Int]): Long = mergeSort(a.clone, 0, a.length, a)
 
-    // binary search on the left side
-    // O(log n)
-    def bsearch(upper: Int, gt: Int): Int = {
-      var (l, r) = (0, upper);
-      while (l != r) {
-        val m = (l + r) / 2
-        if (a(m) <= gt) {
-          l = m+1
-        } else {
-          r = m
-        }
-      }
-      l
+  // O(n•log n)
+  // [l, r)
+  def mergeSort(aux: Array[Int], l: Int, r: Int, result: Array[Int]): Long =
+    if (r - l < 2)
+      0L
+    else {
+      val m = (l + r) / 2
+      mergeSort(result, l, m, aux) + mergeSort(result, m, r, aux) + merge(aux, l, m, r, result)
     }
 
-    it.zipWithIndex.foldLeft(0L) {
-      case (inv, (n, i)) if i == 0 || a(i-1) <= n =>
-        a(i) = n
-        inv
+  def merge(aux: Array[Int], l: Int, m: Int, r: Int, result: Array[Int]) =
+    (l until r).foldLeft((l, m, 0L)) {
+      case ((i, j, swaps), k) if i < m && (j >= r || aux(i) <= aux(j)) =>
+        result(k) = aux(i)
+        (i + 1, j, swaps)
 
-      case (inv, (n, i)) =>
-        val j = bsearch(i - 1, n) //a.indexWhere(_ > n)
-        Array.copy(a, j, a, j+1, i-j) // O(n)
-        a(j) = n
-        inv + i - j
-    }
-  }
+      case ((i, j, swaps), k) =>
+        result(k) = aux(j)
+        (i, j + 1, swaps + m  - i)
+    }._3
 
   def main(args: Array[String]) {
     val scanner = new java.util.Scanner(System.in)
     for (_ <- 0 until scanner.nextInt) {
       val d = scanner.nextInt
-      val it = Iterator.fill(d) { scanner.nextInt }
-      System.out.println(calc(d, it))
+      val a = Array.fill(d) { scanner.nextInt }
+      System.out.println(calc(a))
     }
   }
 }
